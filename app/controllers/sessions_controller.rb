fa0e8c167baba5ea4fcb86ca298f8c22b7758f82
ne241099c <ne241099@senshu-u.jp>
2026-01-06 21:57:18 +0900

    ログインできなくなった

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,20 @@
 class SessionsController < ApplicationController
-  def create
-        member = Member.find_by(email: params[:session][:email])
-        if member&.authenticate(params[:session][:password])
-          cookies_login(member.id)
-        else
-          flash.alert = "名前とパスワードが一致しません"
-        end
-        redirect_to :account, notice: "ログインしました"
-    end
+  def new
+  end
 
-    def destroy
-        cookies.delete(:member_id)
-        redirect_to :root
-    end
+  def create
+    member = Member.find_by(email: params[:session][:email])
+      if member&.authenticate(params[:session][:password])
+        cookies_login(member.id)
+        redirect_to :account, notice: "ログインしました"
+      else
+        flash.alert = "名前とパスワードが一致しません"
+		render :new
+      end
+  end
+
+  def destroy
+    cookies.delete(:member_id)
+    redirect_to :root
+  end
 end
