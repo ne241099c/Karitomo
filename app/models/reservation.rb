982f1bed2b32652e80cf5d805fda3facce1c41c6
ne241099c <ne241099@senshu-u.jp>
2026-01-08 02:52:02 +0900

    修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,7 +5,7 @@ class Reservation < ApplicationRecord
 
   enum status: { pending: 0, approved: 1, rejected: 2, completed: 3 }
 
-  validate :check_availability
+  validate :check_availability, on: :create
   validates :start_at, presence: true
   validates :hours, presence: true, numericality: { greater_than: 0 }
 
